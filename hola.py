7ff752794b0431751655128a81e64a00980f1ca6
Roger Javier Murillo Orozco <rogermurillo7474@gmail.com>
2026-09-15 21:22:25 -0600

    ejercicios

--- hola.py
+++ hola.py
@@ -4,3 +4,14 @@ este es un poema
 que se encuentra en un archivo de python
 y que se puede imprimir en la consola"""
 print(poema.replace("poema", "parabola "))
+
+nombre = "roger"
+ciudad = "managua"
+mensaje = f"hola {nombre}, bienvenido a {ciudad}"
+print(mensaje)
+
+producto = (" computadora laptop    ")
+print(producto.strip().upper())
+
+otros = "Aprender Python es divertido"
+print(len(otros))
