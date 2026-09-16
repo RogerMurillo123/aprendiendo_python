194a3ddbf02e472769ede5f202f22baedfad7655
Roger Javier Murillo Orozco <rogermurillo7474@gmail.com>
2026-09-15 20:40:31 -0600

    estudios

--- hola.py
+++ hola.py
@@ -1,1 +1,6 @@
-print("hola mundo está funcionando")
+
+poema = """
+este es un poema
+que se encuentra en un archivo de python
+y que se puede imprimir en la consola"""
+print(poema.replace("poema", "parabola "))
